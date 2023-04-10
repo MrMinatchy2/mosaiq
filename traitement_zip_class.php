085fc08271e88ef5b2568ae4c45128f7f2fdc98f
MrMinatchy2 <43653960+MrMinatchy2@users.noreply.github.com>
2023-04-10 15:11:48 -0400

    10/04/2022

--- traitement_zip_class.php
+++ traitement_zip_class.php
@@ -1,5 +1,26 @@
 <?php
 class TraitementZip{
+    private $emplacement;
+
+    protected function parcourir_dossier($dossier) {
+        // Ouvrir le dossier
+        $handle = opendir($dossier);
+    
+        // Parcourir le dossier
+        while (false !== ($entry = readdir($handle))) {
+            // Vérifier si l'entrée est un fichier
+            if (is_file("$dossier/$entry")) {
+                // Vérifier si le nom de fichier correspond à la regex
+                if (preg_match('/.*/', $entry)) {
+                    // Afficher le nom de fichier
+                    echo "$entry<br>";
+                }
+            }
+        }
+    
+        // Fermer le dossier
+        closedir($handle);
+    }
     
 }
 
